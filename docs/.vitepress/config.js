fee6f6fe2cb17ae05452ff9f65455c8f28909e3e
hongliang <hongl_gitee@163.com>
2022-08-04 19:41:38 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: 'Hello VitePress',
+  description: 'Just playing around.',
+  themeConfig: {
+    siteTitle: "hongl's record",
+    logo: '/icon.png',
+    nav: [
+      { text: 'Start', link: '/'},
+      // { text: 'AboutMe', link: 'https://github.com/sudongyuer' },
+    ],
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/sudongyuer/learn-eslint' }
+    // ],
+    // editLink: {
+    //   pattern: 'https://github.com/sudongyuer/learn-eslint/tree/master/packages/learn-eslint-doc/docs/:path',
+    //   text: 'Edit this page on GitHub'
+    // },
+    // lastUpdatedText:'最后更新',
+    // algolia: {
+    //   appId: 'B3ZJMMNZBF',
+    //   apiKey: '577e17bf553f9716a78b840040c54804',
+    //   indexName: 'learn-eslint'
+    // },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present sudongyuer'
+    },
+    sidebar: [
+      {
+        text: '记录',
+        collapsible: true,
+        items:[
+          {
+            text: '正则表达式手册',
+            link: '/guide/regexp'
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+layout: home
+
+title: ESLint 通关小册
+titleTemplate: Vite & Vue Powered Static Site Generator
+
+hero:
+  name: hongl's records
+  text: records
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/regexp.html
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/sudongyuer/learn-eslint
+
+features:
+  - title: "记录手册"
+    details: 记录工具中用到的一些工具
+---
