ddaf1694b631e06ea4060c574f8c04984c27ffda
hongliang <hongl_gitee@163.com>
2022-09-09 23:21:01 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,15 +16,15 @@ export default {
     // socialLinks: [
     //   { icon: 'github', link: 'https://github.com/hongl/learn-eslint' }
     // ],
-    // editLink: {
-    //   pattern: 'https://github.com/hongl/learn-eslint/tree/master/packages/learn-eslint-doc/docs/:path',
-    //   text: 'Edit this page on GitHub'
-    // },
+    editLink: {
+      pattern: 'https://github.com/hongl-1/record/tree/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
     // lastUpdatedText:'最后更新',
     // algolia: {
     //   appId: 'B3ZJMMNZBF',
     //   apiKey: '577e17bf553f9716a78b840040c54804',
-    //   indexName: 'learn-eslint'
+    //   indexName: 'ezc'
     // },
     footer: {
       message: 'Released under the MIT License.',
