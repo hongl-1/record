ef52d5bd5c17db0dce64cc9916baf6aff2c5db9f
hongliang <hongl_gitee@163.com>
2023-07-24 10:50:47 +0800

    add interview module

--- docs/.vitepress/catalog/sidebar.js
+++ docs/.vitepress/catalog/sidebar.js
@@ -32,5 +32,51 @@ export const sidebar = [
         link: '/guide/mini-program'
       }
     ]
+  },
+  {
+    text: '前端面试系列',
+    collapsible: true,
+    items:[
+      {
+        text: 'html',
+        link: '/interview/html'
+      },
+      {
+        text: 'css',
+        link: '/interview/css'
+      },
+      {
+        text: 'javascript',
+        link: '/interview/js'
+      },
+      {
+        text: '工具',
+        link: '/interview/tool'
+      },
+      {
+        text: '操作系统',
+        link: '/interview/operationSystem'
+      },
+      {
+        text: '计算机网络',
+        link: '/interview/network'
+      },
+      {
+        text: '算法-剑指offer',
+        link: '/interview/algorithm-jz-offer'
+      },
+      {
+        text: '算法-智力题',
+        link: '/interview/algorithm-qa'
+      },
+      {
+        text: '算法',
+        link: '/interview/algorithm'
+      },
+      {
+        text: '面试记录',
+        link: '/interview/history'
+      }
+    ]
   }
 ]
